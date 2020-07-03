d6acc5c627863320287e7837597724801c26831b
raug-dev24 <romanegustin24@gmail.com>
2020-07-03 14:01:46 -0400

    Updates

---
+++ precache-manifest.920c4fc6a8d49ae62da248f284bc06c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bfb5e740f54f102b63e7ee97f10d6e",
+    "url": "/robostars/index.html"
+  },
+  {
+    "revision": "06113c5bebd2638621e2",
+    "url": "/robostars/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "06113c5bebd2638621e2",
+    "url": "/robostars/static/js/2.2d501e24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robostars/static/js/2.2d501e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8c979c3fcf1081a674",
+    "url": "/robostars/static/js/main.af5b6f0d.chunk.js"
+  },
+  {
+    "revision": "5583b93eaac845a21dbb",
+    "url": "/robostars/static/js/runtime-main.c997a64e.js"
+  }
+]);
