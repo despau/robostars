f30583a538baab20ce9c1a99cb80887a1d6fe58d
raug-dev24 <romanegustin24@gmail.com>
2020-07-03 15:42:46 -0400

    added redux to projec

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList.js';
 import SearchBox from '../components/SearchBox.js';
 import Scroll from '../components/Scroll.js';
 import ErrorBoundry from '../components/ErrorBoundry.js'
 
+import { setSearchField } from '../actions.js';
+
+//
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
+
 class App extends Component {
   constructor() {
     super()
     this.state={
-      robos: [],
-      searchfield: ''
+      robos: []
     }
   };
 
@@ -21,14 +36,17 @@ class App extends Component {
     });
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  }
+  // onSearchChange = (event) => {
+  //   this.setState({ searchfield: event.target.value });
+  // }
 
   render() {
+
+    const { searchField, onSearchChange } = this.props;
+
     const filteredRobos = this.state.robos.filter(robos => {
       //return on this condition true
-      return robos.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
+      return robos.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase());
     });
 
     if(this.state.robos.length === 0) {
@@ -37,7 +55,7 @@ class App extends Component {
       return (
         <div className="tc tc bg-black">
           <h1 className='white'>This is the App</h1>
-            <SearchBox searchChange={this.onSearchChange} />
+            <SearchBox searchChange={onSearchChange} />
             <Scroll>
               <ErrorBoundry>
                 <CardList robos={filteredRobos}/>
@@ -49,4 +67,6 @@ class App extends Component {
   }
 }
 
-export default App;
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './containers/App';
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
+import { searchRobos } from './reducers.js';
+
+const store = createStore(searchRobos)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
